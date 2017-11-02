ea967283a00a53c0d453e764336bc39ad412d708
salott15 <sarilott@yahoo.com>
2017-11-02 14:04:44 -0700

    journal updates

--- src/journals.js
+++ src/journals.js
@@ -18,7 +18,6 @@ class Journals extends Component {
   render() {
   	var journals = []
   	if(this.props.journals.currentJournals){
-  		//journalsArray = 
   	 journals = this.props.journals.currentJournals.map((jrnl, index) => {
   		return <JournalElement {...jrnl} key={index} deleteJournal={(e) => {this.props.deleteJournalElement(e)}} />
 })};
@@ -28,7 +27,7 @@ class Journals extends Component {
   			<h1>TRAVELOGUE</h1>
 
 			<div className="board">
-				<h2>{this.props.name}'s Journals</h2>
+				<h2>{this.props.firstname}&rsquo;s Journals</h2>
 				{journals}
 				<div className="addjournal"><Link to="/newjournal" style={{textDecoration: 'none'}}>
 					<h3>Add a new journal!</h3></Link>
@@ -43,9 +42,9 @@ class Journals extends Component {
   }
 
 const MapStateToProps = function(state, ownProps){
-	console.log(state, state._root.entries[1][1].currentJournals)
-	return {journals: state._root.entries[1][1].currentJournals, 
-		firstname: state._root.entries["0"][1].loggedIn}
+	console.log(state._root.entries)
+	return {journals: state._root.entries[1][1].currentJournals.currentJournals.currentJournals,
+		firstname: state._root.entries["0"][1].loggedIn.name}
 }
 
 const MapDispatchToProps = function(dispatch){
@@ -53,7 +52,7 @@ const MapDispatchToProps = function(dispatch){
 		getUserJournals(dispatch)
 	}, getUser: () => {
 		getUser(dispatch)
-	}, deleteJournalElement: (evt) => { 
+	}, deleteJournalElement: (evt) => {
 		deleteJournal(evt.target.getAttribute("data-jid"), dispatch)
 	}}
 }
