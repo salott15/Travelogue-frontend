091e6395c3121960043d0929a7a361824f269824
salott15 <sarilott@yahoo.com>
2018-01-11 12:53:46 -0800

    readme file

--- src/redux/users/userreducer.js
+++ src/redux/users/userreducer.js
@@ -1,24 +1,24 @@
 const initialState = {
-	name:'notLoggedIn Dude',
-	loggedIn: false,
-	errorMessage: ''
+    name:'notLoggedIn Dude',
+    loggedIn: false,
+    errorMessage: ''
 }
 
 function users(state = initialState, action){
-	switch (action.type)
-	{
-		case 'LOGIN_USER' :
-			return {...state, loggedIn: true, name:action.paylod.username}
+    switch (action.type)
+    {
+        case 'LOGIN_USER' :
+            return {...state, loggedIn: true, name:action.paylod.email}
 
-		case 'GET_USER' : 
-			return {...state, currentUser: action.paylod}
+        case 'GET_USER' :
+            return {...state, currentUser: action.paylod}
 
-		case 'USER_REGISTER_ERROR' : 
-			return {...state, errorMessage: action.error}
+        case 'USER_REGISTER_ERROR' :
+            return {...state, errorMessage: action.error}
 
-		default : 
-			return state;
-	}
+        default :
+            return state;
+    }
 }
 
 export default users;
